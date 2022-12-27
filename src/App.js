cb5252f0904682eb4c9210c5b057a76155c393be
Rudresh Pandey <rudreshpandey2002@gmail.com>
2022-12-27 14:58:22 +0530

    category Function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import './App.css';
 import Anime from './components/Anime';
 import Navbar from './components/Navbar';
 import DemoData from './components/DemoData';
+import { useState } from 'react';
 
 function App() {
+
+  const [animeData, setAnimeData] = useState(DemoData);
+
+  const filterAnime = (category) => {
+    if (category === "All") {
+      setAnimeData(DemoData);
+      return;
+    }
+
+    const updatedAnimeList = DemoData.filter((currEle) => {
+      return currEle.category === category;
+    });
+
+    setAnimeData(updatedAnimeList);
+  }
+
   return (
     <>
-      <Navbar />
-      <Anime DemoData={DemoData} />
+      <Navbar filterAnime={filterAnime} />
+      <Anime DemoData={animeData} />
     </>
   );
 }
--- src/components/DemoData.js
+++ src/components/DemoData.js
@@ -3,11 +3,11 @@ const DemoData = [
         id: 1,
         image: "images/Aot.jpg",
         name: "Attack on Titan",
-        category: "Action",
+        category: "War",
         imdb: "9.8",
         tomato: "95%",
         seasons: "4",
-        eps: "82eps",
+        eps: "82",
         desc: "After his hometown is destroyed and his mother is killed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction."
     },
     {
@@ -18,9 +18,42 @@ const DemoData = [
         imdb: "9.3",
         tomato: "93%",
         seasons: "8",
-        eps: "112eps",
+        eps: "112",
         desc: "The protagonist, Baki Hanma, trains with an intense focus to become strong enough to surpass his father, Yujiro Hanma, the strongest fighter in the world."
-    }
+    },
+    {
+        id: 3,
+        image: "images/deathNote.jpg",
+        name: "Death Note",
+        category: "Thriller",
+        imdb: "9.2",
+        tomato: "90%",
+        seasons: "2",
+        eps: "37",
+        desc: "An intelligent high school student goes on a secret crusade to eliminate criminals from the world after discovering a notebook capable of killing anyone whose name is written into it."
+    },
+    {
+        id: 4,
+        image: "images/onepunchman.jpg",
+        name: "One punch Man",
+        category: "Action",
+        imdb: "8.7",
+        tomato: "100%",
+        seasons: "2",
+        eps: "24",
+        desc: "The story of Saitama, a hero that does it just for fun & can defeat his enemies with a single punch."
+    },
+    {
+        id: 5,
+        image: "images/yourname.jpg",
+        name: "Your Name",
+        category: "Romance",
+        imdb: "8.4",
+        tomato: "98%",
+        seasons: "0",
+        eps: "0",
+        desc: "Two strangers find themselves linked in a bizarre way. When a connection forms, will distance be the only thing to keep them apart?"
+    },
 ];
 
 export default DemoData;
